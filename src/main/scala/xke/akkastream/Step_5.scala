896a42f5326abb3d43b8075d2db74c26a9a57fca
c.verdier <c.verdier.ext@meetic-corp.com>
2016-12-05 03:30:23 +0100

    Step 5

---
+++ src/main/scala/xke/akkastream/Step_5.scala
@@ -0,0 +1,98 @@
+package xke.akkastream
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source}
+import akka.stream.{ActorMaterializer, ClosedShape}
+import xke.akkastream.Step_5.NumberEvent
+
+import scala.util.Random
+
+object Step_5_Run extends App {
+  new Step_5_1_Merge_Broadcast
+}
+
+object Step_5 {
+
+  case class NumberEvent(value: Int)
+}
+
+class Step_5 {
+
+  implicit val system = ActorSystem("step-3")
+  implicit val materializer = ActorMaterializer()
+}
+
+class Step_5_0_Broadcast extends Step_5 {
+
+  lazy val source: Source[NumberEvent, _] = Source.fromIterator(
+    () => Iterator.continually(NumberEvent(Random.nextInt(1000)))
+  )
+
+  lazy val firstSink: Sink[NumberEvent, _] = Sink.foreach(event => {
+    Thread.sleep(50)
+    println(s"First: $event")
+  })
+
+  lazy val secondSink: Sink[NumberEvent, _] = Sink.foreach(event => {
+    Thread.sleep(200)
+    println(s"Second: $event")
+  })
+
+  lazy val thirdSink: Sink[NumberEvent, _] = Sink.foreach(event => {
+    Thread.sleep(100)
+    println(s"Third: $event")
+  })
+
+  lazy val graph = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+    import GraphDSL.Implicits._
+
+    val broadcast = b.add(Broadcast[NumberEvent](3))
+
+    source ~> broadcast ~> firstSink
+              broadcast ~> secondSink
+              broadcast ~> thirdSink
+
+    ClosedShape
+  })
+  graph.run()
+}
+
+class Step_5_1_Merge_Broadcast extends Step_5 {
+
+  lazy val smallNumberSource: Source[NumberEvent, _] = Source.fromIterator(
+    () => Iterator.continually(NumberEvent(Random.nextInt(10)))
+  )
+
+  lazy val mediumNumberSource: Source[NumberEvent, _] = Source.fromIterator(
+    () => Iterator.continually(NumberEvent(Random.nextInt(10) * 10))
+  )
+
+  lazy val largeNumberSource: Source[NumberEvent, _] = Source.fromIterator(
+    () => Iterator.continually(NumberEvent(Random.nextInt(10) * 100))
+  )
+
+  lazy val firstSink: Sink[NumberEvent, _] = Sink.foreach(event => {
+    Thread.sleep(50)
+    println(s"First: $event")
+  })
+
+  lazy val secondSink: Sink[NumberEvent, _] = Sink.foreach(event => {
+    Thread.sleep(100)
+    println(s"Second: $event")
+  })
+
+  lazy val double = Flow[NumberEvent].map(event => NumberEvent(event.value * 2))
+
+  lazy val graph = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+    import GraphDSL.Implicits._
+
+    // TODO: merge all the Sources
+    // TODO: double the NumberEvent's values via 'double'
+    // TODO: broadcast to the two Sinks
+
+    ???
+
+    ClosedShape
+  })
+  graph.run()
+}
---
+++ src/test/scala/xke/akkastream/Step_5_Spec.scala
@@ -0,0 +1,35 @@
+package xke.akkastream
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Sink, Source}
+import akka.testkit.TestProbe
+import org.scalatest.WordSpec
+import xke.akkastream.Step_5.NumberEvent
+
+class Step_5_Spec extends WordSpec {
+
+  implicit val system = ActorSystem("test-step-5")
+
+  "Step5_1_Merge_Broadcast" should {
+    "merge map and broadcast" in {
+
+      val firstSinkResult = TestProbe()
+      val secondSinkResult = TestProbe()
+
+      val step = new Step_5_1_Merge_Broadcast {
+        override lazy val smallNumberSource: Source[NumberEvent, _] = Source(List(NumberEvent(2), NumberEvent(4), NumberEvent(6)))
+        override lazy val mediumNumberSource: Source[NumberEvent, _] = Source(List(NumberEvent(10), NumberEvent(30), NumberEvent(50)))
+        override lazy val largeNumberSource: Source[NumberEvent, _] = Source(List(NumberEvent(300), NumberEvent(900)))
+        override lazy val firstSink: Sink[NumberEvent, _] = Sink.actorRef(firstSinkResult.ref, Done)
+        override lazy val secondSink: Sink[NumberEvent, _] = Sink.actorRef(secondSinkResult.ref, Done)
+      }
+
+      firstSinkResult.expectMsgAllOf(NumberEvent(4), NumberEvent(8), NumberEvent(12), NumberEvent(20), NumberEvent(60), NumberEvent(100), NumberEvent(600), NumberEvent(1800))
+      secondSinkResult.expectMsgAllOf(NumberEvent(4), NumberEvent(8), NumberEvent(12), NumberEvent(20), NumberEvent(60), NumberEvent(100), NumberEvent(600), NumberEvent(1800))
+      firstSinkResult.expectMsg(Done)
+      secondSinkResult.expectMsg(Done)
+    }
+  }
+
+}
