a06e05ff5459bc6a2c58c62c5e81c22c29899b17
c.verdier <c.verdier.ext@meetic-corp.com>
2016-12-05 03:17:26 +0100

    Step 1

---
+++ src/main/scala/xke/akkastream/Step_1.scala
@@ -0,0 +1,110 @@
+package xke.akkastream
+
+import java.util.UUID
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import xke.akkastream.Step_1.{CategorizedEvent, EndEvent, NumberEvent, StartEvent}
+
+import scala.concurrent.Future
+import scala.util.{Failure, Random, Success}
+
+
+object Step_1_Run extends App {
+  // Run a step here
+  new Step_1_0_Helloworld
+}
+
+object Step_1 {
+
+  case class NumberEvent(value: Int)
+
+  case class StartEvent(value: Int)
+  case class EndEvent(value: Int)
+
+  case class CategorizedEvent(category: Int, content: String)
+}
+
+class Step_1 {
+
+  implicit val system = ActorSystem("step-1")
+  implicit val materializer = ActorMaterializer()
+}
+
+class Step_1_0_Helloworld extends Step_1 {
+
+  val source: Source[String, _] = Source.fromIterator(
+    () => Iterator.continually().map(_ => s"Hello, world !")
+  )
+  val sink: Sink[String, _] = Sink.foreach(
+    element => println(element)
+  )
+
+  source.runWith(sink)
+}
+
+class Step_1_1_Source extends Step_1 {
+
+  // TODO: create a Source that will generate 'NumberEvent' with a random value
+  // TODO: you can use 'scala.util.Random.nextInt()'
+  val source: Source[NumberEvent, _] = ???
+
+  val sink: Sink[NumberEvent, _] = Sink.foreach(event => {
+    Thread.sleep(50)
+    println(event)
+  })
+
+  // TODO: run Step_1_1_Source, do you notice the back-pressure ?
+  source.runWith(sink)
+}
+
+class Step_1_2_Map extends Step_1 {
+
+  lazy val source: Source[StartEvent, _] = Source.fromIterator(
+    () => Iterator.from(1).map(StartEvent)
+  )
+
+  lazy val sink: Sink[EndEvent, _] = Sink.foreach(event => {
+    Thread.sleep(50)
+    println(event)
+  })
+
+  // TODO: write a map function so EndEvent's value will be the double of StartEvent's value
+  lazy val stream = source
+    .map(???)
+    .runWith(sink)
+}
+
+class Step_1_3_Filter extends Step_1 {
+
+  lazy val source: Source[CategorizedEvent, _] = Source.fromIterator(
+      () => Iterator.continually().map(_ => CategorizedEvent(Random.nextInt(5), UUID.randomUUID().toString))
+    )
+  lazy val sink: Sink[CategorizedEvent, _] = Sink.foreach(event => {
+    Thread.sleep(50)
+    println(event)
+  })
+
+  // TODO: write a filter function so only CategorizedEvent with category equals 3 are logged by the Sink
+  lazy val stream = source
+    .filter(???)
+    .runWith(sink)
+}
+
+class Step_1_4_MaterializedValue extends Step_1 {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  // TODO: create a Source that will generate exactly 10 'NumberEvent's with a random value
+  lazy val source: Source[NumberEvent, _] = ???
+
+  // TODO: create a Sink that will sum the 'NumberEvent's values on completion
+  lazy val sink: Sink[NumberEvent, Future[Int]] = ???
+
+  lazy val result: Future[Int] = source.runWith(sink)
+  result.onComplete {
+    case Success(value) => println(s"Result: $value")
+    case Failure(error) => println(s"Error: $error")
+  }
+}
+
---
+++ src/test/scala/xke/akkastream/Step_1_Spec.scala
@@ -0,0 +1,84 @@
+package xke.akkastream
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.testkit.TestSubscriber.Probe
+import akka.stream.testkit.scaladsl.{TestSink, TestSource}
+import org.scalatest.{Matchers, WordSpec}
+import xke.akkastream.Step_1.{CategorizedEvent, EndEvent, NumberEvent}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class Step_1_Spec extends WordSpec with Matchers {
+
+  implicit val system = ActorSystem("test-step-1")
+  implicit val materializer = ActorMaterializer()
+
+  "Step_1_1_Source" should {
+    "have a Source that generates NumberEvents" in {
+
+      val source = (new Step_1_1_Source).source
+      val stream = source.runWith(TestSink.probe)
+
+      val event1 = stream.request(1).expectNext()
+      val event2 = stream.request(1).expectNext()
+
+      event1.value should not equal(event2.value)
+    }
+  }
+
+  "Step_1_2_Map" should {
+    "map StartEvent to EndEvent with a doubled value" in {
+
+      val stream = new Step_1_2_Map {
+        override lazy val sink: Sink[EndEvent, _] = TestSink.probe
+      }.stream.asInstanceOf[Probe[EndEvent]]
+
+      val events = stream.request(3).expectNextN(3)
+
+      events should contain theSameElementsInOrderAs(Seq(EndEvent(2), EndEvent(4), EndEvent(6)))
+    }
+  }
+
+  "Step_1_3_Filter" should {
+    "filter CategorizedEvents with a category of 3" in {
+
+      val sourceEvents = List(CategorizedEvent(1, "a"), CategorizedEvent(3, "b"), CategorizedEvent(4, "c"),
+        CategorizedEvent(3, "d"), CategorizedEvent(2, "e"))
+
+      val stream = new Step_1_3_Filter {
+        override lazy val source: Source[CategorizedEvent, _] = Source(sourceEvents)
+        override lazy val sink: Sink[CategorizedEvent, _] = TestSink.probe
+      }.stream.asInstanceOf[Probe[CategorizedEvent]]
+
+      val events = stream.request(5).expectNextN(2)
+
+      events should contain theSameElementsInOrderAs(Seq(CategorizedEvent(3, "b"), CategorizedEvent(3, "d")))
+    }
+  }
+
+  "Step_1_4_MaterializedValue" should {
+    "have a Source that generates exactly 10 NumberEvents" in {
+
+      val source = (new Step_1_4_MaterializedValue).source
+      val stream = source.runWith(TestSink.probe)
+
+      stream.request(10).expectNextN(10)
+      stream.expectComplete()
+    }
+
+    "have a Sink that sums the NumberEvent's values on completion" in {
+      val sink = (new Step_1_4_MaterializedValue).sink
+      val (stream, result) = TestSource.probe[NumberEvent]
+        .toMat(sink)(Keep.both)
+        .run()
+
+      Range(0, 10).foreach(i => stream.unsafeSendNext(NumberEvent(i)))
+      stream.sendComplete()
+
+      Await.result(result, 2 seconds) should be (Range(0, 10).sum)
+    }
+  }
+}
